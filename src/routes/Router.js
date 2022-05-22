ed34dbfd726c578624d32b6044b5569143c33127
Gridy <reality023@naver.com>
2022-05-23 00:39:04 +0900

    라우터 파일 구조 구성

---
+++ src/routes/Router.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NotFound from "./NotFound";
+import Main from "./Main";
+import Detail from "./Detail";
+import Login from "./Login";
+import Register from "./Register";
+import Write from "./Write";
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login">
+                    <Login></Login>
+                </Route>
+                <Route path="/register">
+                    <Register></Register>
+                </Route>
+                <Route path="/detail/:id">
+                    <Detail></Detail>
+                </Route>
+                <Route path="/write">
+                    <Write></Write>
+                </Route>
+                <Route path="/" exact>
+                    <Main></Main>
+                </Route>
+                <Route>
+                    <NotFound></NotFound>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
