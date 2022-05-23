802b137ba7d6e962ead9f20f581b0a2bbff85240
Gridy <reality023@naver.com>
2022-05-23 19:13:18 +0900

    Theme 적용 및 리셋 코드 SCSS 파일로 분리

---
+++ src/styles/theme.js
@@ -0,0 +1,31 @@
+const color = {
+    blue: '#4286C5',
+    black: '#444444',
+    grey: '#777777',
+    white: '#FFFFFF',
+    red: '#E91E63',
+}
+
+const bgColor = {
+    blue: '#2D71AF',
+}
+
+const fontFamily = {
+    default: 'NEXON Lv1 Gothic OTF',
+}
+
+export const defaultTheme = {
+    color,
+    bgColor,
+    fontFamily,
+};
+
+/*
+export const darkTheme = {
+
+}
+
+export const lightTheme = {
+
+}
+*/
