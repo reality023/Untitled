de3a8001e954ecf6b5d429645c6a4e87e0994142
Gridy <reality023@naver.com>
2022-05-23 21:07:36 +0900

    리덕스 기본 세팅 완료

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Router from './routes/Router';
 import { defaultTheme } from './styles/theme';
 import common from './styles/common.scss';
+import { Provider } from 'react-redux';
+import store from './redux/configStore';
+
 
 function App() {
   // 나중에 여러 테마 적용할 때 아래 코드 사용
   // const [theme, setTheme] = useState(defaultTheme);
   const theme = defaultTheme;
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
---
+++ src/redux/configStore.js
@@ -0,0 +1,7 @@
+import {createStore, combineReducers} from "redux";
+import post from "./modules/post";
+
+const rootReducer = combineReducers({post});
+const store = createStore(rootReducer);
+
+export default store;
